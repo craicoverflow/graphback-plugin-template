6507f000b7754d6510f45649d818583272d641f8
Enda Phelan <ephelan@redhat.com>
2020-03-15 22:19:13 +0000

    chore: remove strongly typed config

--- src/MyBlankPlugin.ts
+++ src/MyBlankPlugin.ts
@@ -2,14 +2,11 @@
 import { GraphbackCoreMetadata, GraphbackPlugin } from '@graphback/core'
 import { GraphQLSchema } from 'graphql';
 
-export interface MyBlankPluginOptions {
-}
-
 export const PLUGIN_NAME = "MyBlankPlugin";
 
 export class MyBlankPlugin extends GraphbackPlugin {
 
-    public constructor(pluginConfig?: MyBlankPluginOptions) {
+    public constructor(pluginConfig?: any) {
         super()
     }
 
